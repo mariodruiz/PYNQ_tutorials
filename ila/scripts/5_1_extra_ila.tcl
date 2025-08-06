5f39395a7f47418453425e19b2d6a60d476460bb
Mario Ruiz <11815099+mariodruiz@users.noreply.github.com>
2025-08-06 14:36:57 +0100

    Fix ILA config

--- ila/scripts/5_1_extra_ila.tcl
+++ ila/scripts/5_1_extra_ila.tcl
@@ -2,12 +2,13 @@ set system_ila_1 [ create_bd_cell -type ip -vlnv xilinx.com:ip:system_ila system
 
 set_property -dict [list \
   CONFIG.C_EN_STRG_QUAL {1} \
-] $system_ila_0
+] $system_ila_1
 
 
 connect_bd_net [get_bd_pins system_ila_1/clk] [get_bd_pins zynq_ultra_ps_e/pl_clk0]
 connect_bd_net [get_bd_pins system_ila_1/resetn] [get_bd_pins rst_ps8_100M/peripheral_aresetn]
 connect_bd_intf_net [get_bd_intf_pins system_ila_1/SLOT_0_AXI] [get_bd_intf_pins axi_smc/S01_AXI]
 
+save_bd_design
 validate_bd_design
 save_bd_design
